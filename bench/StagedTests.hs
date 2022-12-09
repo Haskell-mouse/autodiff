cd1d4ff4ab9bd6ad810a50dbbb19e4384d849114
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2022-12-09 10:14:11 -0300

    Save in versioning many different attempts to test staged AD

--- bench/StagedTests.hs
+++ bench/StagedTests.hs
@@ -48,18 +48,118 @@ import Env
 -- Although it typechecks, its expansion takes too long to compile.
 testAdIO ::
   ( SizedSemiring d,
-    forall tup. Semigroup (d tup),
-    (forall n m. (KnownNat n, KnownNat m) => Lift (d '(n, m)))
+    forall tup. Semigroup (d tup)
   ) =>
   AutoDiffStagedType ->
-  IO (SizedNet d) ->
   IO (Mat '(784, 1)) ->
-  SpliceQ (IO (DMap SizedNetVar d))
-testAdIO adFunc netIO inputIO = liftCode $ do
+  SpliceQ (IO (SizedNet d) -> IO (DMap SizedNetVar d))
+testAdIO adFunc inputIO = liftCode $ do
   input <- runIO inputIO
-  examineCode [|| do
-    net <- netIO
-    return $$(adFunc (\var -> CSpliceQ [|| ($$(splice var) `var2Getter` net) ||]) (exprSizedTestStaged input)) ||]
+  examineCode
+    [||
+    \netIO -> do
+      net <- netIO
+      return $$(adFunc (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) (exprSizedTestStaged input))
+    ||]
+
+testReverseAdIO ::
+  ( SizedSemiring d,
+    forall tup. Semigroup (d tup)
+  ) =>
+  IO (Mat '(784, 1)) ->
+  SpliceQ (IO (SizedNet d) -> IO (DMap SizedNetVar d))
+testReverseAdIO inputIO = liftCode $ do
+  input <- runIO inputIO
+  let expression = exprSizedTestStaged input
+  examineCode
+    [||
+    \netIO -> do
+      net <- netIO
+      return $$(reverseADStaged (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) expression)
+    ||]
+
+testReverseAdIO2 ::
+  ( SizedSemiring d,
+    forall tup. Semigroup (d tup)
+  ) =>
+  SpliceQ (SizedNet d -> DMap SizedNetVar d)
+testReverseAdIO2 = liftCode $ do
+  input <- runIO setupSizedInput
+  let expression = exprSizedTestStaged input
+  examineCode
+    [||\net -> $$(reverseADStaged (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) expression)||]
+
+-- testReverseAdIO3 ::
+--   ( SizedSemiring d,
+--     forall tup. Semigroup (d tup)
+--   ) =>
+--   SpliceQ ((SizedNet d, Mat '(784, 1)) -> IO (DMap SizedNetVar d))
+-- testReverseAdIO3 =
+--   [||\(net, input) -> $$(reverseADStaged (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) exprSizedTestStaged input)||]
+
+-- testReverseAdIO ::
+--   ( SizedSemiring d,
+--     forall tup. Semigroup (d tup),
+--     (forall n m. (Lift (d '(n, m))))
+--   ) =>
+--   IO (Mat '(784, 1)) ->
+--   IO (SizedNet d) ->
+--   SpliceQ (IO (DMap SizedNetVar d))
+-- testReverseAdIO inputIO netIO = liftCode $ do
+--   input <- runIO inputIO
+--   net <- runIO netIO
+--   examineCode
+--     [||
+--       return $$(reverseADStaged (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) (exprSizedTestStaged input))
+--     ||]
+
+-- almost!
+--
+-- testReverseAdIO ::
+--   ( SizedSemiring d,
+--     forall tup. Semigroup (d tup)
+--   ) =>
+--   IO (Mat '(784, 1)) ->
+--   IO (SizedNet d) -> IO (DMap SizedNetVar d)
+-- testReverseAdIO = 
+--     $$([||
+--     \inputIO netIO -> do
+--       input <- inputIO
+--       net <- netIO
+--       return $$(reverseADStaged (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) (exprSizedTestStaged input))
+--     ||])
+
+-- testAd ::
+--   ( SizedSemiring d,
+--     forall tup. Semigroup (d tup),
+--     (forall n m. (KnownNat n, KnownNat m) => (Lift (d '(n, m))))
+--   ) =>
+--   AutoDiffStagedType ->
+--   SizedNet d ->
+--   Mat '(784, 1) ->
+--   SpliceQ (DMap SizedNetVar d)
+-- testAd adFunc net input =
+--   [||$$(adFunc (\var -> CSpliceQ [||($$(splice var) `var2Getter` net)||]) (exprSizedTestStaged input))||]
+
+-- testAd ::
+--   ( SizedSemiring d,
+--     forall tup. Semigroup (d tup),
+--     (forall n m. (KnownNat n, KnownNat m) => Lift (d '(n, m)))
+--   ) =>
+--   AutoDiffStagedType ->
+--   SizedNet d ->
+--   Mat '(784, 1) ->
+--   SpliceQ (DMap SizedNetVar d)
+-- testAd adFunc net input = [|| $$(adFunc (\var -> CSpliceQ [|| ($$(splice var) `var2Getter` net) ||]) (exprSizedTestStaged input)) ||]
+
+-- testReverseAd ::
+--   ( SizedSemiring d,
+--     forall tup. Semigroup (d tup),
+--     (forall n m. (KnownNat n, KnownNat m) => Lift (d '(n, m)))
+--   ) =>
+--   SizedNet d ->
+--   DMap SizedNetVar d
+-- testReverseAd net = $$(reverseADStaged (\var -> CSpliceQ [|| ($$(splice var) `var2Getter` net) ||]) (exprSizedTestStaged (unsafePerformIO setupSizedInputStaged)))
 
 -- testAdIO ::
 --   ( SizedSemiring d,
